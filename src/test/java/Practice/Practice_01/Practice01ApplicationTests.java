f83a8b3c676cbe0581b474a1beee050a6cd43d02
iamkhj18 <iamkhj18@naver.com>
2021-04-21 14:55:12 +0900

    프로젝트 생성

---
+++ src/test/java/Practice/Practice_01/Practice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practice.Practice_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
