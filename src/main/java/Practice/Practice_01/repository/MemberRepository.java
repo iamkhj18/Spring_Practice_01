d4816c754785051ce4e0cd0505f4c8a9be3f8f0c
iamkhj18 <iamkhj18@naver.com>
2021-04-21 16:31:07 +0900

    회원서비스 개발 및 테스트

---
+++ src/main/java/Practice/Practice_01/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package Practice.Practice_01.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import Practice.Practice_01.domain.Member;
+
+public interface MemberRepository {
+	Member save(Member member);
+
+	Optional<Member> findById(Long id);
+
+	Optional<Member> findByName(String name);
+
+	List<Member> findAll();
+}
